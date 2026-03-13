8c3dafc115565ef086e02afafcc9a835f407e2ae
Pramudito Nawa Satria <pramuditonawasatria@gmail.com>
2026-03-13 14:43:42 +0700

    menambahkan void input

--- Insertiton_sort.cpp
+++ Insertiton_sort.cpp
@@ -1,3 +1,30 @@
 #include <iostream>
 using namespace std;
 
+int arr[20];
+int n;
+
+void input(){
+  while(true){
+    cout <<  "Masukan Jumlah Data Pada Array : ";
+    cin >> n;
+  
+     if (n <=20){
+            break;
+        }
+        else{
+            cout << "\nArray tidak boleh lebih dari 20\n";
+        }
+    }
+    cout << endl;
+    cout << "===============================";
+    cout << "======Masukan Elemen Array=====";
+    cout << "===============================";
+
+    for (int i = 0; i < n; i++ ){
+        cout << "Data Ke-" << (i + 1) << ": ";
+        cin >> arr[i];
+    }
+  }
+
+  
