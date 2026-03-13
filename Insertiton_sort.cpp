19ac56533d94ebf5731427b83bcb67590c1bd505
Pramudito Nawa Satria <pramuditonawasatria@gmail.com>
2026-03-13 14:52:49 +0700

    Menambah VoiDIsplay

--- Insertiton_sort.cpp
+++ Insertiton_sort.cpp
@@ -50,3 +50,14 @@ void input(){
     }
 } 
 
+void display(){
+    cout << endl;
+    cout << "=================";
+    cout << "\nEelement Array" << endl;
+    cout << "=================";
+
+    for(int j = 0; j < n; j++){
+        cout << arr{j} << endl;
+    }
+    cout << endl;
+}
